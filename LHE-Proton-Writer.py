7121c900548410744974200e229049dd202aa59e
Artur Uhlik Frohlich <artur.uhlik@gmail.com>
2024-08-07 13:58:54 -0300

    Updated Proton Writer
    Now reads and writes event by event and doesn't load the entire .lhe.
    Now accepts more than one type of particle as source for protons

--- LHE-Proton-Writer.py
+++ LHE-Proton-Writer.py
@@ -4,14 +4,14 @@ from ROOT import TLorentzVector
 Writes the protons into an LHE file from  MadGraph or Superchic 
 '''
 # Receives particle ID, file name and generator of origin as arguments
-if len(sys.argv) != 4:
+if len(sys.argv) < 4:
     print('Missing argument, correct syntax as follows:')
-    print('python3 LHE-Proton-Writer.py <ID> <path of .lhe file> <generator of origin>')
+    print('python3 LHE-Proton-Writer.py <path of .lhe file> <generator of origin> <IDs>')
     sys.exit()
 
-ID = str(sys.argv[1])
-path = sys.argv[2]
-generator = sys.argv[3].lower()
+path = sys.argv[1]
+generator = sys.argv[2].lower()
+ID = sys.argv[3:]
 new = 'new'+path
 
 # Flag for printing the Invariant Mass of protons and leptons
@@ -21,21 +21,20 @@ printivm = False
 if(generator == 'madgraph'):
     flag0 = '<event>\n'
     flag1 = '</event>\n'
-    header = '</header>\n'
+    header = '<init>\n'
+    end = '</LesHouchesEvents>\n'
 if(generator == 'superchic'):
     flag0 = ' <event>\n'
     flag1 = ' </event>\n'
-    header = ' </header>\n'
+    header = ' <init>\n'
+    end = ' </LesHouchesEvents>\n'
 
 # Create list of lines from the LHE file and if needed finding the beginning of events 
 with open(path, 'r+') as f:
-    lines = f.readlines()
-    if(generator == 'madgraph'):
-        lines.index(flag0)
-        beamenergy = int(float(lines[lines.index(header)+2].split()[2]))
-    if(generator == 'superchic'):
-        lines.index(flag0)
-        beamenergy = int(float(lines[lines.index(header)+2].split()[2]))
+    l = f.readline()
+    while l != header:
+        l = f.readline()
+    beamenergy = int(float(f.readline().split()[2]))
 
 pzini = beamenergy		# protons inicial pz 
 eini = beamenergy		# protons inicial energy
@@ -43,52 +42,58 @@ eini = beamenergy		# protons inicial energy
 # Proton rest mass: 0.9382720882E+00    # from: https://pdg.lbl.gov/2019/reviews/rpp2019-rev-phys-constants.pdf
 m0 = 0.9382720882
 
+
+event = []
 lvector = TLorentzVector()
-with open(new, 'w') as new:
-    i = 0
-    while True:
-        if lines[i] == flag0:
-            j = 0
-            while lines[i+j] != flag1:
-                if lines[i+j].split()[0] == ID and lines[i+j].split()[1] == '-1':
-                    line = lines[i+j].split()
-                    px = f'{-eval(lines[i+j].split()[6]):.9e}' if -eval(line[6]) < 0 else f'+{-eval(lines[i+j].split()[6]):.9e}'
-                    py = f'{-eval(lines[i+j].split()[7]):.9e}' if -eval(line[7]) < 0 else f'+{-eval(lines[i+j].split()[7]):.9e}'
+with open(path, 'r+') as f, open(new, 'w') as new:
+    l = f.readline()
+    while l != end:
+        l = f.readline()
+        if l == flag0:
+            event.append(l)
+            while l != flag1:
+                l = f.readline()
+                event.append(l)
+            for i in range(len(event)):
+                if event[i].split()[0] in ID and event[i].split()[1] == '-1':
+                    # Updating number of particles on header
+                    neweventheader = event[1].split()
+                    if generator == 'superchic':
+                        neweventheader[0] = ' '+str(int(neweventheader[0])+1)
+                        event.pop(1)
+                        event.insert(1, '    '.join(neweventheader)+'\n')
+                    if generator == 'madgraph':
+                        neweventheader[0] = ' '+str(int(neweventheader[0])+1)+'     '
+                        event.pop(1)
+                        event.insert(1, ' '.join(neweventheader)+'\n')
+                    # Creating the 4-momentum vector
+                    line = event[i].split()
+                    px = f'{-eval(line[6]):.9e}' if -eval(line[6]) < 0 else f'+{-eval(line[6]):.9e}'
+                    py = f'{-eval(line[7]):.9e}' if -eval(line[7]) < 0 else f'+{-eval(line[7]):.9e}'
                     pzf = eval(line[8])
                     sign = (pzf/abs(pzf))
                     pzp = pzini*sign - pzf
                     ef = eval(line[9])
                     ep = eini - ef
                     if generator == 'superchic':
-                        if sign > 0: lines.insert(i+j+2, ' '*13+f'2212{" "*8}1    0    0    0    0 {px} {py} +{pzp:.9e}  {ep:.9e}  {m0:.9e} 0. 1.\n')
-                        else: lines.insert(i+j+2, ' '*13+f'2212{" "*8}1    0    0    0    0 {px} {py} {pzp:.9e}  {ep:.9e}  {m0:.9e} 0. -1.\n')
+                        if sign > 0: event.insert(i+1, ' '*13+f'2212{" "*8}1    0    0    0    0 {px} {py} +{pzp:.9e}  {ep:.9e}  {m0:.9e} 0. 1.\n')
+                        else: event.insert(i+1, ' '*13+f'2212{" "*8}1    0    0    0    0 {px} {py} {pzp:.9e}  {ep:.9e}  {m0:.9e} 0. -1.\n')
                     if generator == 'madgraph':
-                        if sign > 0: lines.insert(i+j+2, ' '*5+f'2212{" "*2}1    0    0    0    0 +{0:.10e} +{0:.10e} +{pzp:.10e} {ep:.10e} {m0:.10e} {0:.4e} {1:.4e}\n')
-                        else: lines.insert(i+j+2, ' '*5+f'2212{" "*2}1    0    0    0    0 -{0:.10e} -{0:.10e} {pzp:.10e} {ep:.10e} {m0:.10e} {0:.4e} {-1:.4e}\n')
-                j += 1
-            neweventheader = lines[i+1].split()
-            if generator == 'superchic':
-                neweventheader[0] = ' '+str(int(neweventheader[0])+2)
-                lines.pop(i+1)
-                lines.insert(i+1, '    '.join(neweventheader)+'\n')
-            if generator == 'madgraph':
-                neweventheader[0] = ' '+str(int(neweventheader[0])+2)+'     '
-                lines.pop(i+1)
-                lines.insert(i+1, ' '.join(neweventheader)+'\n')
-        i += 1
-        if i == len(lines): break
-    for line in lines:
-        if(printivm):
-            line = line.strip().split()
-            if(len(line) > 1 and line[0] == '2212'):
-                lvector.SetPxPyPzE(float(line[6]), float(line[7]), float(line[8]), float(line[9]))
-                print(f'M(p) = {lvector.M()}')
-            if(len(line) > 1 and line[0] == '13'):
-                lvector.SetPxPyPzE(float(line[6]), float(line[7]), float(line[8]), float(line[9]))
-                print(f'M(\u03BC+) = {lvector.M()}')
-            if(len(line) > 1 and line[0] == '-13'):
-                lvector.SetPxPyPzE(float(line[6]), float(line[7]), float(line[8]), float(line[9]))
-                print(f'M(\u03BC) = {lvector.M()}\n*-*-*-*-*-*-*-*-*-*-*')
-        new.write(line)
-
-
+                        if sign > 0: event.insert(i+1, ' '*5+f'2212{" "*2}1    0    0    0    0 +{0:.10e} +{0:.10e} +{pzp:.10e} {ep:.10e} {m0:.10e} {0:.4e} {1:.4e}\n')
+                        else: event.insert(i+1, ' '*5+f'2212{" "*2}1    0    0    0    0 -{0:.10e} -{0:.10e} {pzp:.10e} {ep:.10e} {m0:.10e} {0:.4e} {-1:.4e}\n')
+            for nl in event:
+                new.write(nl)
+                if(printivm):
+                   line = nl.split()
+                   if(len(line) > 1 and line[0] == '2212'):
+                       lvector.SetPxPyPzE(float(line[6]), float(line[7]), float(line[8]), float(line[9]))
+                       print(f'M(p) = {lvector.M()}')
+                   if(len(line) > 1 and line[0] == '13'):
+                       lvector.SetPxPyPzE(float(line[6]), float(line[7]), float(line[8]), float(line[9]))
+                       print(f'M(\u03BC+) = {lvector.M()}')
+                   if(len(line) > 1 and line[0] == '-13'):
+                       lvector.SetPxPyPzE(float(line[6]), float(line[7]), float(line[8]), float(line[9]))
+                       print(f'M(\u03BC) = {lvector.M()}\n*-*-*-*-*-*-*-*-*-*-*')
+            event.clear()
+        else:
+            new.write(l)
